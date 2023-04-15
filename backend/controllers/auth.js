f9f290ef522a4efb8269dd813bb4199a613fbf2e
Orchi1904 <alexej1@web.de>
2023-04-15 02:39:09 +0200

    Changed register controller a bit

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,30 +1,38 @@
 import { db } from "../connect.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
 
 export const register = (req, res) => {
     const q = "SELECT * FROM users WHERE username = ? OR email = ?";
+    const {username, email, password, repPassword} = req.body;
 
-    db.query(q, [req.body.username, req.body.email], (err, data) => {
+    db.query(q, [username, email], (err, data) => {
         //Check if there is an error or the user already exists
         if (err) return res.status(500).json(err)
-        if (data.length) return res.status(409).json("Der Benutzer existiert bereits!");
+        if (data.length) return res.status(422).json({msg: "Der Benutzer existiert bereits!"});
 
         //Check if password and repPassword are the same
-        if(req.body.password !== req.body.repPassword)
+        if(password !== repPassword)
         return res.status(500).json("Die Passwörter stimmen nicht überein!");
 
         //Hash password of new user
         const salt = bcrypt.genSaltSync(10);
-        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+        const hashedPassword = bcrypt.hashSync(password, salt);
 
         const q = "INSERT INTO users (`username`, `email`, `password`) VALUES (?)";
 
-        const values = [req.body.username, req.body.email, hashedPassword];
+        const values = [username, email, hashedPassword];
+
+        const accessToken = jwt.sign({ username }, "verysecretkey");
+
+        /*res.json({
+            accessToken,
+        });*/
 
         db.query(q, [values], (err, data) => {
             if (err) return res.status(500).json(err);
-            return res.status(200).json("Der Nutzer wurde erfolgreich angelegt!");
+            return res.status(200).json({accessToken, msg: "Der Nutzer wurde erfolgreich angelegt!"});
         });
     });
 };
